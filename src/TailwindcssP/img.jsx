54d35037d7af59345e8bc3305ef43b4f5ee83662
hasanali78678 <hassanali78678ha@gmail.com>
2024-11-14 23:12:49 +0500

    home cover slider in btn

--- src/TailwindcssP/img.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-export default function Img(){
-    return(
-        <React.Fragment>
-            <img src="https://gamehub.om/wp-content/uploads/2023/06/CFI-ZCT1W.jpg" alt="controllerps5" />
-        </React.Fragment>
-    );
-} 
--- src/UBISOFTGames/CardData/coverSliderData.jsx
+++ src/UBISOFTGames/CardData/coverSliderData.jsx
@@ -1,12 +1,12 @@
-const coverSlider =[
+const coverSlider = [
     {
         id: 1,
         image: 'https://staticctf.ubisoft.com/J3yJr34U2pZ2Ieem48Dwy9uqj5PNUQTn/6B49sRNBG3UNrxVHuPMtqc/b250112469ae05ccaa26ef1d9a6c024a/ubi-homescreen-afop-keyart-v1.jpg',
         clogo: 'https://staticctf.ubisoft.com/J3yJr34U2pZ2Ieem48Dwy9uqj5PNUQTn/5Bp8eQtQWRJoK6dqcAuaQ/11e0246d682e1ae7c79e54d34d511ced/Ubisoft_SignetTag_006EF5_Agnostic.png',
         title: "PROTECT PANDORA. BECOME NA'VI.",
         paragraph: "Reconnect with your lost heritage, discover what it truly means to be Na'vi, and join other clans to protect Pandora.'",
-        btnC: "lg:w-[230px]  rounded-full lg:text-[15px] lg:h-[50px] text-white bg-sky-600 md:w-[260px] md:h-[50px] md:mx-5 md:text-[17px] xs:w-[160px] xs:h-[40px] xs:mx-2 xs:text-[14px]",
-        btn1: 'WHACT TRAILER',
+        btnC: "lg:w-[230px]  rounded-full lg:text-[15px] lg:h-[50px] text-white bg-sky-600 md:w-[260px] md:h-[50px] md:mx-5 md:text-[17px] xs:w-[160px] xs:h-[40px] xs:mx-2 xs:text-[14px] border-sky-600 hover:border-sky-600 transition-all duration-300 border-2 hover:text-sky-600 hover:bg-white",
+        btn1: 'WATCH TRAILER',
         btn2: 'BUY NOW',
         cgamelogo: 'https://staticctf.ubisoft.com/J3yJr34U2pZ2Ieem48Dwy9uqj5PNUQTn/6ZRnFq8it72gilq0V1obMK/7f90f174d818384f3e20b323eb00e1f7/AVATAR_Frontiers_of_Pandora_LOGO_RGB_White.png'
     },
@@ -16,8 +16,8 @@ const coverSlider =[
         clogo: 'https://staticctf.ubisoft.com/J3yJr34U2pZ2Ieem48Dwy9uqj5PNUQTn/5Bp8eQtQWRJoK6dqcAuaQ/11e0246d682e1ae7c79e54d34d511ced/Ubisoft_SignetTag_006EF5_Agnostic.png',
         title: "OPERATION DEEP FREEZE",
         paragraph: "This season we welcome Tubarão, a Defender sent from sunny Portugal to help you thaw the cold spirits of winter.",
-        btnC: "lg:w-[230px]  rounded-full lg:text-[15px] lg:h-[50px] text-white bg-[#2d3031] md:w-[260px] md:h-[50px] md:mx-5 md:text-[17px] xs:w-[160px] xs:h-[40px] xs:mx-2 xs:text-[14px]",
-        btn1: 'WHACT TRAILER',
+        btnC: "lg:w-[230px]  rounded-full lg:text-[15px] lg:h-[50px] text-white bg-[#2d3031] md:w-[260px] md:h-[50px] md:mx-5 md:text-[17px] xs:w-[160px] xs:h-[40px] xs:mx-2 xs:text-[14px] border-[#2d3031] hover:border-[#2d3031] transition-all duration-300 border-2 hover:text-[#2d3031] hover:bg-white",
+        btn1: 'WATCH TRAILER',
         btn2: 'BUY NOW',
         cgamelogo: 'https://staticctf.ubisoft.com/J3yJr34U2pZ2Ieem48Dwy9uqj5PNUQTn/53XHaP3QiX6PFa08DEsM36/17115ad721bb937bc76a7fa0737bb647/r6s-y6-logo__1_.png'
     },
@@ -27,8 +27,8 @@ const coverSlider =[
         clogo: 'https://staticctf.ubisoft.com/J3yJr34U2pZ2Ieem48Dwy9uqj5PNUQTn/5Bp8eQtQWRJoK6dqcAuaQ/11e0246d682e1ae7c79e54d34d511ced/Ubisoft_SignetTag_006EF5_Agnostic.png',
         title: "SKULL AND BONES IS AVAILABLE NOW !",
         paragraph: "Get Skull & Bones now to join the everlasting pirate adventure! Available on PlayStation®5, Xbox Series X|S and PC, or with a Ubisoft+ subscription.",
-        btnC: "lg:w-[230px]  rounded-full lg:text-[15px] lg:h-[50px] text-white bg-[#fbbf21] md:w-[260px] md:h-[50px] md:mx-5 md:text-[17px] xs:w-[160px] xs:h-[40px] xs:mx-2 xs:text-[14px]",
-        btn1: 'WHACT TRAILER',
+        btnC: "lg:w-[230px]  rounded-full lg:text-[15px] lg:h-[50px] text-white bg-[#fbbf21] md:w-[260px] md:h-[50px] md:mx-5 md:text-[17px] xs:w-[160px] xs:h-[40px] xs:mx-2 xs:text-[14px] border-[#fbbf21] hover:border-[#fbbf21] transition-all duration-300 border-2 hover:text-[#fbbf21] hover:bg-white",
+        btn1: 'WATCH TRAILER',
         btn2: 'BUY NOW',
         cgamelogo: 'https://staticctf.ubisoft.com/J3yJr34U2pZ2Ieem48Dwy9uqj5PNUQTn/69dYrpvDKz2pvtv1V4tFlq/febc8bceff14fef1d74f20a2e7bc9439/SB-Logo-InLine-tm-UbiOriginal_FINAL_WHITE.png'
     },
@@ -38,8 +38,8 @@ const coverSlider =[
         clogo: 'https://staticctf.ubisoft.com/J3yJr34U2pZ2Ieem48Dwy9uqj5PNUQTn/5Bp8eQtQWRJoK6dqcAuaQ/11e0246d682e1ae7c79e54d34d511ced/Ubisoft_SignetTag_006EF5_Agnostic.png',
         title: "BUCKLE UP FOR SEASON 2!",
         paragraph: "Meet Brian Scotto and the HOONIGAN crew, and get ready to kill all tires!",
-        btnC: "lg:w-[230px]  rounded-full lg:text-[15px] lg:h-[50px] text-white bg-[#cf152d] md:w-[260px] md:h-[50px] md:mx-5 md:text-[17px] xs:w-[160px] xs:h-[40px] xs:mx-2 xs:text-[14px]",
-        btn1: 'WHACT TRAILER',
+        btnC: "lg:w-[230px]  rounded-full lg:text-[15px] lg:h-[50px] text-white bg-[#cf152d] md:w-[260px] md:h-[50px] md:mx-5 md:text-[17px] xs:w-[160px] xs:h-[40px] xs:mx-2 xs:text-[14px] border-[#cf152d] hover:border-[#cf152d] transition-all duration-300 border-2 hover:text-[#cf152d] hover:bg-white",
+        btn1: 'WATCH TRAILER',
         btn2: 'BUY NOW',
         cgamelogo: 'https://staticctf.ubisoft.com/J3yJr34U2pZ2Ieem48Dwy9uqj5PNUQTn/57nkMqYX3qNoYJlB2u7IGO/42d505062780e4ebdb7f5f9de9bf04fe/ubi-homescreen-logo-crew_motorfest.png'
     },
@@ -49,8 +49,8 @@ const coverSlider =[
         clogo: 'https://staticctf.ubisoft.com/J3yJr34U2pZ2Ieem48Dwy9uqj5PNUQTn/5Bp8eQtQWRJoK6dqcAuaQ/11e0246d682e1ae7c79e54d34d511ced/Ubisoft_SignetTag_006EF5_Agnostic.png',
         title: 'YEAR 5 SEASON 3 "VANGUARD" & PROJECT RESOLVE',
         paragraph: "There's never been a better time to become an agent in The Division. Learn about the new season, its story, and the thousands of fixes and improvements!",
-        btnC: "lg:w-[230px]  rounded-full lg:text-[15px] lg:h-[50px] text-white bg-[#fd6d11] md:w-[260px] md:h-[50px] md:mx-5 md:text-[17px] xs:w-[160px] xs:h-[40px] xs:mx-2 xs:text-[14px]",
-        btn1: 'WHACT TRAILER',
+        btnC: "lg:w-[230px]  rounded-full lg:text-[15px] lg:h-[50px] text-white bg-[#fd6d11] md:w-[260px] md:h-[50px] md:mx-5 md:text-[17px] xs:w-[160px] xs:h-[40px] xs:mx-2 xs:text-[14px] border-[#fd6d11] hover:border-[#fd6d11] transition-all duration-300 border-2 hover:text-[#fd6d11] hover:bg-white",
+        btn1: 'WATCH TRAILER',
         btn2: 'BUY NOW',
         cgamelogo: 'https://staticctf.ubisoft.com/J3yJr34U2pZ2Ieem48Dwy9uqj5PNUQTn/4dV2dLwUptokYWvNEMBoh9/d9b81a65a5a779fa6b5d8cf39045ff05/ubi-homescreen-logo-division2-white-v1.png'
     }
